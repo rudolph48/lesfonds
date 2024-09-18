bf5bce8c216fce15f43fc4c83de5da4876d70cec
rudolph48 <rudolph12241@gmail.com>
2024-09-18 12:23:39 +0800

    First commit of my website

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,25 +1,29 @@
 // components/Footer.tsx
 import styles from './Footer.module.css';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
         <div className={styles.column}>
-          <h3>首頁</h3>
-          <p>關於工作室</p>
+          <Link href='/' passHref>
+            <p>首頁</p>
+          </Link>
+          <Link href='/about' passHref>
+            <p>關於工作室</p>
+          </Link>
           <p>當季甜點介紹</p>
         </div>
         <div className={styles.column}>
-          <h3>聯繫我們</h3>
           <p>Line: @lesfonds</p>
-          <p>Email: lesfondsdepatisserie@gmail.com</p>
+          <p>信箱: lesfondsdepatisserie@gmail.com</p>
           <p>公司名稱:土半夏甜點工作室 94479357</p>
           <p>以上產品皆已投保產品責任保險</p>
           <p>食品業登錄字號：B-202269941-00000-3</p>
         </div>
         <div className={styles.column}>
-          <h3>訂購與配送服務</h3>
+          <p>訂購與配送服務</p>
           <p>追蹤貨品狀態</p>
           <p>條款與細則</p>
         </div>
